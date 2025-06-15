dfe7630ff4dfb068c44363d6f24c1653c5f17b9d
Fahiye Muhammad <fahiye254@gmail.com>
2025-06-15 12:51:57 +0300

    Learnign Creating and Retreiving using flask REST API

--- server/app.py
+++ server/app.py
@@ -16,12 +16,59 @@ db.init_app(app)
 
 api = Api(app)
 
+class Home(Resource):
+    def get(self):
+
+        return {"Success": "Hello Welcome to the plant REST API"},200
+    
+api.add_resource(Home, '/')    
+    
+
 class Plants(Resource):
-    pass
+    def get(self):
+        plants_dict = [ plant.to_dict() for plant in Plant.query.all()]
+
+        if plants_dict:
+            return plants_dict,200
+        else:
+            return {"error": "Plant not found"}, 404
+        
+    def post(self):
+
+        data = request.get_json()
+        try:
+            new_plant = Plant(
+                name=data["name"],
+                image=data["image"],
+                price= float(data["price"]),
+            )
+        except (KeyError, TypeError, ValueError):
+            return {"error": "Invalid input"}, 400    
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        return new_plant.to_dict(), 201
+
+api.add_resource(Plants, '/plants')    
+
+        
+
 
 class PlantByID(Resource):
-    pass
+    def get(self,id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        if plant:
+            return plant.to_dict(),200
+        else:
+            return {"error": "Plant not found"}, 404
+
         
+    
+api.add_resource(PlantByID, '/plants/<int:id>') 
+
+       
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
